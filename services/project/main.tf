a7b529756d4fa9f50b65055799f3329acbeec1de
Mikael Kalms <mikael@kalms.org>
2020-06-30 09:32:00 +0200

    Firewall rule depends on Compute Engine API being enabled, with extra sleep
    Enabling APIs in GCP seems to be 'eventually consistent' - even though the API claims to be enabled, trying to use it immediately after receiving OK can result in errors. Adding a 30-second forced delay appears to avoid the problem, according to the internet

--- services/project/main.tf
+++ services/project/main.tf
@@ -2,9 +2,24 @@ module "google_apis" {
   source = "./google_apis"
 }
 
+# Wait 30 seconds between enabling Google APIs and attempting to use them
+#
+# While the google_project_service resource does wait for the API to become enabled,
+#   the enabling is in practice eventually consistent - and some calls may fail even
+#   after GCP claims that the API is free to use.
+#
+# See https://github.com/terraform-providers/terraform-provider-google/issues/2129 for some discussion
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [module.google_apis]
+
+  create_duration = "30s"
+}
+
 resource "google_compute_firewall" "allow_winrm" {
-  name    = "allow-winrm"
-  network = "default"
+  depends_on = [time_sleep.wait_30_seconds]
+  name       = "allow-winrm"
+  network    = "default"
 
   allow {
     protocol = "tcp"
