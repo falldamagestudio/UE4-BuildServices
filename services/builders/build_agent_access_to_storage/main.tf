c9784f3cf421b1085034e282999be79d2b93c210
Mikael Kalms <mikael@kalms.org>
2020-07-06 11:59:47 +0200

    Use iam_member instead of iam_binding for granting bucket access
    iam_member allows composition (multiple unrelated systems can be granted access to the same bucket via different Terraservices), iam_binding requires the entire member list to be available at once

--- services/builders/build_agent_access_to_storage/main.tf
+++ services/builders/build_agent_access_to_storage/main.tf
@@ -1,8 +1,6 @@
-resource "google_storage_bucket_iam_binding" "this" {
+resource "google_storage_bucket_iam_member" "this" {
   for_each = toset(var.storage_bucket_ids)
   bucket = each.value
   role = "roles/storage.admin"
-  members = [
-    "serviceAccount:${var.build_agent_email}",
-  ]
+  member = "serviceAccount:${var.build_agent_email}"
 }
